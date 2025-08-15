01a8093dac505e57a3784fdab11b54c0b77dba89
Monkfuego <lakogamhac@gmail.com>
2025-08-15 22:29:13 +0530

    slight changes

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
-const app = require('./index');
+const index = require('./index');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB Connected');
-    app.listen(process.env.PORT, () => {
+    index.listen(process.env.PORT, () => {
       console.log(`🚀 Server running on port ${process.env.PORT}`);
     });
   }).catch((err) => console.error('❌ MongoDB Error:', err));
